c81d64c0b5b23c9af44efe030fb28d79ebe8227d
Tom Maenan Read Cutting <tom.read-cutting@electricsquare.com>
2019-11-23 11:17:47 +0000

    Maximum throbbing

--- JamTest/scripts/megacube.gd
+++ JamTest/scripts/megacube.gd
@@ -6,7 +6,7 @@ var move_dir := Vector2(0, 0)
 
 func _process(delta: float) -> void:
 	var spatialMat := csgmesh.material as SpatialMaterial
-	spatialMat.albedo_color = Color(sin(OS.get_ticks_msec()*0.01)*0.5+0.5, sin(OS.get_ticks_msec()*0.02+2.515)*0.5+0.5, sin(OS.get_ticks_msec()*0.002+0.121561)*0.5+0.5);
+
 
 	move_dir =  Vector2(0, 0)
 
@@ -19,11 +19,12 @@ func _process(delta: float) -> void:
 	if(Input.get_action_strength("move_right") > 0):
 		move_dir += Vector2(1, 0)
 
-	if move_dir.length_squared() > 0:
-		spatialMat.albedo_color = Color(0, 0, 0)
+
+	var throbSpeed : float = 0.001
+	throbSpeed = throbSpeed + 0.001 * move_dir.length_squared()
+	spatialMat.albedo_color = Color(sin(OS.get_ticks_msec()*throbSpeed)*0.5+0.5, sin(OS.get_ticks_msec()*throbSpeed*2+2.515)*0.5+0.5, sin(OS.get_ticks_msec()*throbSpeed*0.2+0.121561)*0.5+0.5);
 
 func _physics_process(delta: float) -> void:
-
 	apply_central_impulse(Vector3(move_dir.x, 0, move_dir.y) * mass)
 
 	if(Input.get_action_strength("jump") > 0):
