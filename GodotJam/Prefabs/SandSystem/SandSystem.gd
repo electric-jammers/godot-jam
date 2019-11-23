c13681f4560092471b6aec5f00f8008174003a10
Tom Maenan Read Cutting <tom.read-cutting@electricsquare.com>
2019-11-23 14:01:26 +0000

    Center the sand and make it more error-resilient

--- GodotJam/Prefabs/SandSystem/SandSystem.gd
+++ GodotJam/Prefabs/SandSystem/SandSystem.gd
@@ -9,6 +9,8 @@ var size_x : int = 100
 var size_y : int = 100
 var size_z : int = 100
 
+var root_position = Vector3(50, 0, 50)
+
 enum SandState {
 	NONE,
 	SAND
@@ -18,28 +20,28 @@ var cube_spatial_dict = {}
 
 onready var cube_scene = load("res://Prefabs/SandSystem/SandCube.tscn")
 
-func position_to_int_position(position: Vector3) -> PoolIntArray:
-	var arr := PoolIntArray()
-	arr.append(int(position.x))
-	arr.append(int(position.y))
-	arr.append(int(position.z))
-	return arr
-
 func index_to_world_position(index: int) -> Vector3:
+	# warning-ignore:integer_division
 	var y := index / (size_x * size_z)
+	# warning-ignore:integer_division
 	var z := (index - (y * size_x * size_z)) / size_x
 	var x := index - ((y * size_x * size_z) + (z * size_x))
 	var result := Vector3(x, y, z)
-
-	return result
+	return result - root_position
 
 func position_to_index(position: Vector3) -> int:
-	var int_positions := position_to_int_position(position)
-	return int(int_positions[0]) + (int(int_positions[1]) * size_x * size_z) + (int(int_positions[2]) * (size_x))
+	position = position + root_position
+	var x := int(position.x)
+	var y := int(position.y)
+	var z := int(position.z)
+	return int(x) + (int(y) * size_x * size_z) + (int(z) * (size_x))
 
 
 func add_sand(position: Vector3) -> void:
 	var position_index := position_to_index(position)
+	if position_index < 0 or position_index >= sand_voxels.size():
+		print("Trying to remove sand outside of the sand bounds!")
+		return
 	sand_voxels.set(position_to_index(position), SandState.SAND)
 	damage.set(position_to_index(position), 0)
 	var cube : Spatial = cube_scene.instance()
@@ -49,6 +51,9 @@ func add_sand(position: Vector3) -> void:
 
 func remove_sand(position: Vector3) -> void:
 	var position_index := position_to_index(position)
+	if position_index < 0 or position_index >= sand_voxels.size():
+		print("Trying to remove sand outside of the sand bounds!")
+		return
 	sand_voxels.set(position_to_index(position), SandState.NONE)
 	if cube_spatial_dict.has(position_index):
 		var cube : Spatial = cube_spatial_dict[position_index]
@@ -66,5 +71,5 @@ func _ready() -> void:
 
 	for x in size_x:
 		for z in size_z:
-			add_sand(Vector3(x, 0, z))
+			add_sand(Vector3(x, 0, z) - root_position)
 
