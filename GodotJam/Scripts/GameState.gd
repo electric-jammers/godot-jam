319cba13b83e69d29496e20b3d944d12211c02a7
Joe Flood <joe@tinygoose.co.uk>
2019-11-23 15:35:34 +0000

    Add signals and counters to gamestate

--- GodotJam/Scripts/GameState.gd
+++ GodotJam/Scripts/GameState.gd
@@ -1,13 +1,29 @@
 extends Node
 
+# Emitted when stage changes
+signal stage_changed(new_stage)
+
 enum GameStage {
 	FRONTEND,
+
 	DAY,
 	NIGHT
 }
 
 var current_time := 0.0
+var time_in_stage := 0.0
+
 var stage = GameStage.FRONTEND
+
+func _process(delta: float):
+	if stage == GameStage.FRONTEND:
+		return
+
+	current_time += delta
+	time_in_stage += delta
 
 func enter_stage(new_game_stage):
 	stage = new_game_stage
+	emit_signal("stage_changed", new_game_stage)
+
+	time_in_stage = 0.0
