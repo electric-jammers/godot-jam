b66f319af7d82c1b1ca49fd6eec1c6824469f4a2
rxlecky <35656626+rxlecky@users.noreply.github.com>
2019-11-23 19:37:58 +0000

    call randomize() in RandomizedStreamPlayer

--- GodotJam/Prefabs/RandomizedStreamPlayer/RandomizedStreamPlayer.gd
+++ GodotJam/Prefabs/RandomizedStreamPlayer/RandomizedStreamPlayer.gd
@@ -2,6 +2,9 @@ extends AudioStreamPlayer
 
 export(Array, AudioStream) var stream_array
 
+func _ready() -> void:
+	randomize()
+
 func play(from_position := 0.0):
 	var sound_index = randi() % stream_array.size()
 
