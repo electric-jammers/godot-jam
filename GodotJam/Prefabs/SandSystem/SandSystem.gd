682332c4915c459f71e467de096aed19a8e1c4ec
Tom Maenan Read Cutting <tom.read-cutting@electricsquare.com>
2019-11-23 13:32:04 +0000

    Spawn them cubes!

--- GodotJam/Prefabs/SandSystem/SandSystem.gd
+++ GodotJam/Prefabs/SandSystem/SandSystem.gd
@@ -2,7 +2,8 @@ extends Node
 
 class_name SandSystem
 
-var arr :=  PoolByteArray ()
+var sand_voxels :=  PoolByteArray ()
+var damage := PoolByteArray()
 
 var size_x : int = 100
 var size_y : int = 100
@@ -15,6 +16,8 @@ enum SandState {
 
 var cube_spatial_dict = {}
 
+onready var cube_scene = load("res://Prefabs/SandSystem/SandCube.tscn")
+
 func position_to_int(position: Vector3) -> PoolIntArray:
 	var arr := PoolIntArray()
 	arr.append(int(position.x))
@@ -22,6 +25,12 @@ func position_to_int(position: Vector3) -> PoolIntArray:
 	arr.append(int(position.z))
 	return arr
 
+func fixed_position(position: Vector3) -> Vector3:
+	var x := int(position.x)
+	var y := int(position.y)
+	var z := int(position.z)
+	var result := Vector3(x, y, z)
+	return result
 
 func position_to_index(position: Vector3) -> int:
 	var int_positions := position_to_int(position)
@@ -29,17 +38,34 @@ func position_to_index(position: Vector3) -> int:
 
 
 func add_sand(position: Vector3) -> void:
-	var position_index = position_to_index(position)
-	arr.set(position_to_index(position), SandState.SAND)
-	# cube_spatial_dict[position_index] = # new spawned cube
+	var position_index := position_to_index(position)
+	sand_voxels.set(position_to_index(position), SandState.SAND)
+	damage.set(position_to_index(position), 0)
+	var cube : Spatial = cube_scene.instance()
+	add_child(cube)
+	cube_spatial_dict[position_index] = cube
+	cube.translation = fixed_position(position)
+
+func remove_sand(position: Vector3) -> void:
+	var position_index := position_to_index(position)
+	sand_voxels.set(position_to_index(position), SandState.NONE)
+	if cube_spatial_dict.has(position_index):
+		var cube : Spatial = cube_spatial_dict[position_index]
+		cube.queue_free()
+		cube_spatial_dict.erase(position_index)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	arr.resize(size_x * size_y * size_z)
-	for index in arr.size():
-		arr.set(index, SandState.NONE)
+	sand_voxels.resize(size_x * size_y * size_z)
+	damage.resize(size_x * size_y * size_z)
+	for index in sand_voxels.size():
+		sand_voxels.set(index, SandState.NONE)
+		damage.set(index, 0)
 
-	print(arr.size())
+	print(sand_voxels.size())
+
+	add_sand(Vector3(0, 0, 0))
 
 	# var mesh := Mesh.new()
 	pass # Replace with function body.
