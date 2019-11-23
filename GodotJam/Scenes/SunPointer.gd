202dc3fd986b259b5a57089195474713e79b6bc7
AJ Weeks <ajweeks@shaw.ca>
2019-11-23 21:08:50 +0000

    Fade directional lights in and out

--- GodotJam/Scenes/SunPointer.gd
+++ GodotJam/Scenes/SunPointer.gd
@@ -9,11 +9,16 @@ func _ready():
 
 func _process(delta):
 	look_at(Vector3.ZERO, Vector3.UP);
-	var time = fmod(GameState.get_normalized_day_night_cycle_time(), 1.0);
+	var time = fmod(GameState.get_normalized_day_night_cycle_time(), 1.0)
+	var stageTime = GameState.get_normalized_stage_time()
+
+
 	if (isSun and (time > 0.75 or time < 0.25)) or (not isSun and (time > 0.25 and time < 0.75)):
 		light_energy = 0.0
 		shadow_enabled = false
 	else:
-		light_energy = initialLightEnergy
+		light_energy = pow(lerp(0.0, initialLightEnergy, 1.0 - abs(stageTime - 0.5) * 2.0), 0.2);
 		shadow_enabled = true
 
+	if isSun:
+		print(String(light_energy))
