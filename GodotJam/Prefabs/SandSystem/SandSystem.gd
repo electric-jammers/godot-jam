42d96b9de91a566006999e6d11c442aa55cf7cc6
Tom Maenan Read Cutting <tom.read-cutting@electricsquare.com>
2019-11-23 12:16:29 +0000

    Create sand system

--- GodotJam/Prefabs/SandSystem/SandSystem.gd
+++ GodotJam/Prefabs/SandSystem/SandSystem.gd
@@ -2,6 +2,10 @@ extends Node
 
 class_name SandSystem
 
+var arr =  []
+
+var size = Vector3(0, 0, 0)
+
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
@@ -9,6 +13,7 @@ class_name SandSystem
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	arr.resize(size.x * size.y * size.z)
 	pass # Replace with function body.
 
 
