bc52ab03bcf736423ac9def2623e24ed9902c825
rxlecky <35656626+rxlecky@users.noreply.github.com>
2019-11-23 19:01:49 +0000

    Add prefab to play random sounds

---
+++ GodotJam/Prefabs/RandomizedStreamPlayer/RandomizedStreamPlayer.gd
@@ -0,0 +1,9 @@
+extends AudioStreamPlayer
+
+export(Array, AudioStream) var stream_array
+
+func play(from_position := 0.0):
+	var sound_index = randi() % stream_array.size()
+
+	stream = stream_array[sound_index]
+	.play(from_position)
